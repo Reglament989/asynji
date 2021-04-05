bb7cf0a2d6309c8450a08bfb0b111406daed05fe
Reglament989 <dudecom@tuta.io>
2021-04-05 20:06:52 +0300

    Pusher as other microservice

--- main.go
+++ main.go
@@ -1,17 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"gin_msg/ws"
-	"log"
-	"net/http"
-	"time"
-
 	"github.com/joho/godotenv"
-	"golang.org/x/sync/errgroup"
-)
-var (
-	g errgroup.Group
 )
 
 func main() {
@@ -23,33 +13,6 @@ func main() {
 	
 	r := InitGin()
 
-	g.Go(func () error{
-		h := ws.InitWs()
-		s := &http.Server{
-			Addr:         ":8081",
-			Handler:      h,
-			ReadTimeout:  5 * time.Second,
-			WriteTimeout: 10 * time.Second,
-		}
-		fmt.Println("\033[32mWs now online on 8081!\033[39m")
-		err := s.ListenAndServe()
-		return err
-	})
-
-	g.Go(func () error {
-		s := &http.Server{
-			Addr:         ":8080",
-			Handler:      r,
-			ReadTimeout:  5 * time.Second,
-			WriteTimeout: 10 * time.Second,
-		}
-		fmt.Println("\033[32mBackend now online on 8080!\033[39m")
-		err := s.ListenAndServe()
-		return err
-	})
-
-	if err := g.Wait(); err != nil {
-		log.Fatal(err)
-	}
+	r.Run()
 	
 }
